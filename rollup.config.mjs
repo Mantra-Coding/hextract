355f905abb9a89b3342ca21bc1ca6e59cf060f8c
lino sena <a.scognamiglio32@studenti.unisa.it>
2025-06-07 22:42:49 +0200

    build: update terser import syntax in rollup config
    The import statement for terser was updated to use the default import syntax instead of named import to match the plugin's actual export style.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,7 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
-import { terser } from "@rollup/plugin-terser";
+import terser from "@rollup/plugin-terser";
 import packageJson from "./package.json" with { type: "json" };
 
 const banner = `/*
